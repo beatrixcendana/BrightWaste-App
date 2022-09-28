61c1826b0f9bd4fd87390674e78457531b7f04ba
Justin Abha Banu Prabowo <justinbp2009@gmail.com>
2022-09-29 00:59:51 +0800

    added

--- pages/submission.js
+++ pages/submission.js
@@ -1,10 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import styles from '../styles/submission.module.css';
 import Swal from 'sweetalert2';
 
 import Head from 'next/head';
 import { userAccessToken, fetchUser } from '../utils/fetchUserDetails';
 
 import { useRouter } from 'next/router';
+
+// import all images
+import Image from 'next/image';
+import Clothing1 from '../public/images/Pict 1_Clothing.png';
+import Electronics2 from '../public/images/Pict 2_Electronics.png';
+import Homegarden3 from '../public/images/Pict 3_Homegarden.png';
+import Automotive4 from '../public/images/Pict 4_Automotive.png';
+import Others5 from '../public/images/Pict 5_Others.png';
+import Paypal from '../public/images/Icon_PayPal.png';
+import Visa from '../public/images/Icon_Visa.png';
+import MasterCard from '../public/images/Icon_mastercard.png';
+// import { useEffect, useState } from 'react';
+
 
 export default function Submission() {
 
@@ -47,16 +61,26 @@ export default function Submission() {
 
                     <div className='row'>
                         {/* isi radio button sama background pake image */}
-                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>box 1</div>
-                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>box 2</div>
-                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>box 3</div>
-                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>box 4</div>
-                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>box 5</div>
+                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>
+                            <Image src={Clothing1} width={188} height={188} className= {styles.images}/>
+                        </div>
+                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>
+                            <Image src={Electronics2} width={188} height={188} className= {styles.images}/>
+                        </div>
+                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>
+                            <Image src={Homegarden3} width={188} height={188} className= {styles.images}/>
+                        </div>
+                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>
+                            <Image src={Automotive4} width={188} height={188} className= {styles.images}/>
+                        </div>
+                        <div className='col-sm-2 col-md-2 col-lg-2 bg-light p-4 me-2'>
+                            <Image src={Others5} width={188} height={188} className= {styles.images}/>
+                        </div>
                     </div>
 
                     <div className='form-group row mt-3 mb-3'>
                         <label>Name of product</label>
-                        <input type='text' placeholder='Book Drawer' className='form-control border-0 bg-light'/>
+                        <input type='text' placeholder='Book Drawer' style={{ backgroundColor: '#469D8C' }} className='form-control border-0 bg-light'/>
                     </div>
 
                     <div className='form-group row mt-3 mb-3'>
@@ -67,46 +91,52 @@ export default function Submission() {
                     <div className='form-group row mt-3 mb-3'>
                         <label>Price offer (in USD)&#63; </label>
                         {/* text field background ubah jd ijo sesuaiin di mockup */}
-                        <input type='number' placeholder='5 pcs' className='form-control border-0 bg-light'/>
+                        <input type='number' placeholder='5 pcs' style={{ backgroundColor: '#469D8C' }} className='form-control border-0 bg-light'/>
                     </div>
 
                     {/* buat css di next js ikutin caranya di halaman getchoices.js */}
                     <div className='form-group row mt-3 mb-3'>
                         <label>Describe the Item (up to 500 words)</label>
-                        <textarea cols="5" rows="5" className='form-control border-0 bg-light'></textarea>
+                        <textarea cols="5" rows="5" style={{ backgroundColor: '#469D8C' }} className='form-control border-0 bg-light'></textarea>
                     </div>
 
                     <div className='form-group row mt-3 mb-3'>
                         <label>Upload the picture of the Item</label>
                         <div className='row'>
                             <div className='col-lg-2 col-sm-2 col-md-2 me-2'>
-                                <input type="file" className='form-control'/>
+                                <input type="file" style={{ backgroundColor: '#469D8C' }}  className='form-control'/>
                             </div>
                             <div className='col-lg-2 col-sm-2 col-md-2 me-2'>
-                                <input type="file" className='form-control'/>
+                                <input type="file" style={{ backgroundColor: '#469D8C' }} className='form-control'/>
                             </div>
                         </div>
                     </div>
 
                     <div className='form-group row mt-3 mb-3'>
                         <div className='col-lg-2 col-md-2 col-sm-2 me-3'>
-                            <input type="radio" />
-                            Paypal logo
+                            <label className= {styles.labelpayment}>
+                                <input type="radio" name="payment" className= {styles.inputRadio}/>
+                                <Image src={Paypal} width={180} height={50} className= {styles.images} alt="Paypal"/>
+                            </label>
                         </div>
                         <div className='col-lg-2 col-md-2 col-sm-2 me-3'>
-                            <input type="radio" />
-                            Visa
+                            <label className= {styles.labelpayment}>
+                                <input type="radio" name="payment" className= {styles.inputRadio}/>
+                                <Image src={Visa} width={142} height={66} className= {styles.images} alt="Visa"/>
+                            </label>
                         </div>
                         <div className='col-lg-2 col-md-2 col-sm-2 me-3'>
-                            <input type="radio" />
-                            Master Card
+                            <label className= {styles.labelpayment}>
+                                <input type="radio" name="payment" className= {styles.inputRadio}/>
+                                <Image src={MasterCard} width={126} height={67} className= {styles.images} alt="Master Card"/>
+                            </label>
                         </div>
                     </div>
 
                     <div className='form-group row mt-3 mb-3'>
                         <label>Location</label>
                         <small>We need to know location so we can find the nearest store for you</small>
-                        <input type="text" placeholder="Lacey, WA, US" className="form-control border-0 bg-light"/>
+                        <input type="text" placeholder="Lacey, WA, US" style={{ backgroundColor: '#469D8C' }} className="form-control border-0 bg-light"/>
                     </div>
 
                     <div className='form-group row mt-4 mb-3'>
